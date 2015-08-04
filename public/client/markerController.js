0bf47baf7f85eda76cdc5d57792098ada31f02ce
Rodolfo Yabut <rodolfoyabut@gmail.com>
2015-08-04 03:20:30 -0700

    [Progress] Add interval to pin drops.

--- public/client/markerController.js
+++ public/client/markerController.js
@@ -7,7 +7,7 @@ angular.module('mapper', [])
 
 .controller('markerCtrl', function($scope, $http) {
   $scope.get = function() {
-    var access = 'https://api.instagram.com/v1/tags/' + $scope.query + '?access_token=26237012.24a20a9.5a528843d38c4aaa99d434aa35af8356';
+    var access = 'https://api.instagram.com/v1/tags/' + $scope.query + '?access_token=26237012.24a20a9.5a528843d38c4aaa99d434aa35af8356&count=100';
     // var access = 'https://api.instagram.com/v1/users/rodoriot/?access_token=26237012.24a20a9.5a528843d38c4aaa99d434aa35af8356';
 
     $http.post('/api', {data: $scope.query})
@@ -15,15 +15,26 @@ angular.module('mapper', [])
       var list = response.data.data;
       var lat; 
       var long;
+      var coordsList = [];
+      var count = 0;
 
       for(var i = 0; i < list.length; i++) {
         if(list[i].location) {
           console.log(list[i])
           lat = list[i].location.latitude;
           long = list[i].location.longitude;
-          marker(lat, long);
+          coordsList.push({lat: lat, long: long});
         }
       }
+
+      setInterval(function(){
+        if(count < coordsList.length) {
+          marker(coordsList[count].lat, coordsList[count].long);
+          count++; 
+        } else {
+          clearInterval();
+        }
+      }, 400);
     });
   };
 });
